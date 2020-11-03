94b9b4418cfa22b42542c37f570cab9d4ced4579
Jerry Crews <jerry.crews1@gmail.com>
2020-11-03 10:22:16 -0500

    Create the shell of the Hotel class.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 """ Hotel Management System.
 """
+
+
+class Hotel:
+    """ Represents the hotel that will be managed.
+
+    Attributes:
+        name (str): The name of the hotel.
+
+    Args:
+        name (str): The name of the hotel.
+    """
+
+    def __init__(self, name):
+        self.name = name
