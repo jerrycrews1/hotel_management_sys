dabbaf3cd72e937378440ab9f97dff191861f073
Jerry Crews <jerry.crews1@gmail.com>
2020-12-16 10:02:01 -0500

    Added option to view all reservations belonging to a specific guest

--- main2.py
+++ main2.py
@@ -28,18 +28,14 @@ def create_guest(name=None, phone_number=None):
     c.execute('SELECT * FROM guests ORDER BY guest_id DESC LIMIT 1')
     guest_phone_number = c.fetchone()[1]
     guest = Guest(guest_phone_number)
-    print(guest)
     return guest
 
 
 def get_guest(phone_number=None):
     if not phone_number:
         phone_number = int(input('Enter guest\'s phone number >'))
-    try:
-        guest = Guest(phone_number)
-        return guest
-    except TypeError:
-        return create_guest(phone_number=phone_number)
+    guest = Guest(phone_number)
+    return guest
 
 
 def edit_guest():
@@ -78,24 +74,33 @@ def get_hotel():
     c.execute('SELECT * FROM hotels')
     hotels = c.fetchall()
     for index, hotel in enumerate(hotels):
-        print(f'{index + 1}:{hotel[1]}')
+        print(f'{index + 1}: {hotel[1]}')
     hotel_id = int(input('Which hotel do you want to manage? >'))
     hotel = Hotel(hotel_id)
     return hotel
 
 
 def create_reservation():
+    ''' Creates a new reservation
+
+    Returns:
+        reservation (obj): A reservation object.
+    '''
     conn = sqlite3.connect('hotel.db')
     c = conn.cursor()
     phone_number = int(input('Enter guest\'s phone number >'))
     try:
+        # If the guest doesn't already exist, move to the except statement.
         guest = get_guest(phone_number=phone_number)
     except TypeError:
-        guest = create_guest(phone_number=get_guest())
+        # Create a new guest from the phone_number already entered.
+        guest = create_guest(phone_number=phone_number)
     guest_id = guest.guest_id
     num_rooms = int(
         int(input("Enter how many rooms the guest needs (no more than 5): ")))
     rooms = list()
+    # Loop through each room the user wants and check if it is available.
+    # If it is, add it to the rooms list, if not, ask again.
     for room in range(num_rooms):
         room_type = input(
             f"Enter the type of room #{room + 1} the guest wants (king, queen, double, single): ").casefold()
@@ -107,25 +112,56 @@ def create_reservation():
                 f"Sorry that room type isn't avialable. Please try again. \nEnter the type of room #{room + 1} the guest wants (king, queen, double, single): ").casefold()
         rooms.append(rooms_available[0])
 
+    # Get the user's check_in date, change to a datetime
     check_in = input('Enter your check in date (Jun 10 2020): ')
     check_in = datetime.datetime.strptime(check_in, '%b %d %Y')
     check_in = check_in.replace(hour=13, minute=00)
+    # Get the number of days stating and create the check_out datetime from that
     days_staying = int(input("Enter how many days the guest be staying: "))
-
     check_out = (check_in + datetime.timedelta(days=days_staying))
     check_out = check_out.replace(hour=11, minute=00)
+
+    # Add the reservation to the db
     insert_statement = 'INSERT INTO reservations (guest_id, check_in, check_out) VALUES (?, ?, ?)'
     c.execute(insert_statement, (guest_id, check_in, check_out))
     conn.commit()
+    # Get the reservation back from the db
     c.execute('SELECT * FROM reservations ORDER BY reservation_id DESC LIMIT 1')
     reservation_id = c.fetchone()[0]
-    print(reservation_id)
     reservation = Reservation(reservation_id)
+    # Loop through each room the user wants and add it to the reservation_has_rooms table in the db.
     for room in rooms:
-        print(room)
         res_to_rooms_insert = 'INSERT INTO reservation_has_rooms VALUES (?, ?)'
         c.execute(res_to_rooms_insert, (reservation.reservation_id, room[0]))
     conn.commit()
+
+    return reservation
+
+
+def get_reservation():
+    conn = sqlite3.connect('hotel.db')
+    c = conn.cursor()
+
+    phone_number = int(input('Enter guest\'s phone number >'))
+    while True:
+        try:
+            # If the guest doesn't already exist, move to the except statement.
+            guest = get_guest(phone_number=phone_number)
+        except ValueError:
+            print('No reservation under that phone number were found.')
+            continue
+        else:
+            break
+    query = 'SELECT * FROM reservations WHERE guest_id = (SELECT guest_id FROM guests WHERE phone_number = ?);'
+    c.execute(query, (phone_number,))
+    # Print the reservations for the specific guests and have them select which reservation by id.
+    reservations = c.fetchall()
+    for index, reservation in enumerate(reservations):
+        print(
+            f'{index + 1}: Check In: {reservation[2]} Check Out {reservation[3]}')
+    reservation_id = int(input('Which reservation do you want to manage? >'))
+    reservation = Reservation(reservation_id)
+    return reservation_id
 
 
 def main():
@@ -158,7 +194,7 @@ def main():
                 break
         elif int(thing) == 2:
             reservation_thing = input(
-                'What do you want to do? \n1. New Reservation,\n2. View Reservation\n3. Alter An Existing Reservation\n4. Cancel Reservation')
+                'What do you want to do? \n1. New Reservation, \n2. View Reservation, \n3. Alter An Existing Reservation, \n4. Cancel Reservation\n>')
             if int(reservation_thing) == 1:
                 create_reservation()
             elif int(reservation_thing) == 2:
